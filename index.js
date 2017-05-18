26e6d0ef3ecc83d2ab840b9a9d103f8759de418d
Dror Ben-Gai <dror@snyk.io>
2017-05-18 12:23:22 +0300

    feat: cleaner code, tests, travis.yml

---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var snykToHtml = require('./lib/snyk-to-html.js');
+var argv = require('minimist')(process.argv.slice(2));
+
+var template, source, output;
+
+if (argv.t) { // template
+  template = argv.t; // grab the next item
+  if (typeof template === 'boolean') {
+    template = __dirname + '/template/test-report.hbs';
+  }
+} else {
+  template = __dirname + '/template/test-report.hbs';
+}
+if (argv.i) { // input source
+  source = argv.i; // grab the next item
+  if (typeof source === 'boolean') {
+    source = undefined;
+  }
+}
+if (argv.o) { // output destination
+  output = argv.o; // grab the next item
+  if (typeof output === 'boolean') {
+    output = undefined;
+  }
+}
+
+snykToHtml.run(source, template, onReportOutput);
+
+function onReportOutput(report) {
+  if (output) {
+    fs.writeFile(output, report, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('Vulnerability snapshot saved at ' + output);
+    });
+  } else {
+    console.log(report);
+  }
+}
