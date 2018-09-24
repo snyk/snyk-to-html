916b421595bfd96dcb34612c4ea47544591d6542
kevinwright <kev.lee.wright@gmail.com>
2018-09-24 17:53:41 +0100

    FEAT: Allows parsing of snyk test output generated from multiple project paths (#21)
    * FEAT: Adds support for multi-project test results
    * Adds path(s) to generated page
    * removes a high-risk dependency imported via the semantic-release module
    * fixes travis syntax for node_js
    * isolates CSS from the main hbs template
    
    (see also: SC-6131)

--- test/snyk-to-html.test.js
+++ test/snyk-to-html.test.js
@@ -13,6 +13,19 @@ test('all-around test', function (t) {
     });
 });
 
+test('multi-report test', function (t) {
+  t.plan(5);
+  snykToHtml.run(
+    __dirname + '/fixtures/multi-test-report.json',
+    __dirname + '/../template/test-report.hbs',
+    function (report) {
+      t.contains(report, '<div class="meta-count"><span>139 vulnerable dependency paths</span></div>');
+      t.contains(report, '<h2 class="card__title">Access Restriction Bypass</h2>');
+      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (ReDoS)<\/h2>');
+      t.contains(report, '<h2 class="card__title">Cross-site Scripting (XSS)</h2>');
+      t.contains(report, '<h2 class="card__title">Regular Expression Denial of Service (DoS)</h2>');
+    });
+});
 
 test('empty values test (description and info)', function (t) {
   t.plan(1);
