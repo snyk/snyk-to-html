6a16a1b9f2700b9f4fa5c298429ccbcb84ba0846
Kevin Wright <kevin@snyk.io>
2018-09-29 13:02:37 +0100

    chore: added a soupçon of documentation to the markdown restructuring logic

--- src/lib/hbars.ts
+++ src/lib/hbars.ts
@@ -25,13 +25,29 @@ export class Hbars {
   }
 }
 
+// Our details markdown for a vuln consists of a series of H2 section headers
+// alternating between some number of paragraphs denoting section content.
+//
+// `marked` will render this by translating the header name to an `id` attribute
+// (which is bad, as it results in multiple duplicate IDs)
+//
+// This function changes that behaviour by wrapping the entirety of each
+// section (including the header) in a `div` element and changing the `id`
+// attribute into a `class` attribute - also adding the `markdown` class.
+//
+// A subsequent client-side transform defined in inline-script.hbs will then
+// lift this class attribute to the surrounding div, allowing for entire
+// selections to be hidden/revealed in script based on that css class.
+function remarked(md: string): string {
+  return marked(md.replace(/\n##/g, '\n§§\n##'))
+    .replace(/id="/g, 'class="markdown ')
+    .split('<p>§§</p>')
+    .map(x => `<div>${x}</div>`)
+    .join('');
+}
 // handlebar helpers
 const hh = {
-  markdown: (md) => marked(md.replace(/\n##/g, '\n§§\n##'))
-                    .replace(/id="/g, 'class="markdown ')
-                    .split('<p>§§</p>')
-                    .map(x => `<div>${x}</div>`)
-                    .join(''),
+  markdown: remarked,
   moment: (date, format) => moment.utc(date).format(format),
   dump: (data, spacer) => JSON.stringify(data, null, spacer || null),
 };
