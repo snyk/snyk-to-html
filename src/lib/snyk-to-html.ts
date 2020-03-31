dabc3d4f30dbe06b2e2d469d819f7a6d7ede8367
ekbsnyk <eleanor.kavanagh-brown@snyk.io>
2020-03-31 16:42:24 +0100

    feat: sort report in descending order of severity and name

--- src/lib/snyk-to-html.ts
+++ src/lib/snyk-to-html.ts
@@ -2,6 +2,7 @@
 
 import chalk from 'chalk';
 import * as debugModule from 'debug';
+import * as _ from 'lodash';
 import fs = require('fs');
 import Handlebars = require('handlebars');
 import marked = require('marked');
@@ -119,7 +120,12 @@ async function registerPeerPartial(templatePath: string, name: string): Promise<
 
 async function generateTemplate(data: any, template: string, summary: boolean): Promise<string> {
   const vulnMetadata = groupVulns(data.vulnerabilities);
-  data.vulnerabilities = vulnMetadata.vulnerabilities;
+  const sortedGroupedVulns = _.orderBy(
+    vulnMetadata.vulnerabilities,
+    ["metadata.severityValue", "metadata.name"],
+    ["desc", "desc"]
+  );
+  data.vulnerabilities = sortedGroupedVulns;
   data.uniqueCount = vulnMetadata.vulnerabilitiesUniqueCount;
   data.summary = vulnMetadata.vulnerabilitiesPathsCount + ' vulnerable dependency paths';
   data.showSummaryOnly = summary;
